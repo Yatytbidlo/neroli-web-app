e86e26c61bacfc182bb6d018e557f3021d775258
Yatytbidlo <prisilaite.suda@gmail.com>
2023-07-20 19:06:29 +0600

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,9 +4,9 @@ import ProductItem from "../ProductItem/ProductItem"
 import {useTelegram} from "../../hooks/useTelegram";
 
 const products = [
-    {id: '1', title: 'Пробник', price: 4990, description: '40 пшиков на 7 дней'},
-    {id: '2', title: '30мл', price: 35000, description: '120 пшиков на 60 дней +в подарок новинка пробник "Grape"'},
-    {id: '3', title: '50мл', price: 75000, description: '200 пшиков на 100 дней +в подарок новинка пробник "Grape"'}
+    {id: '1', title: 'Пробник Neroli', price: 4990, description: '40 пшиков на 7 дней'},
+    {id: '2', title: 'Neroli 30мл', price: 35000, description: '120 пшиков на 60 дней +в подарок новинка пробник "Grape"'},
+    {id: '3', title: 'Neroli 50мл', price: 75000, description: '200 пшиков на 100 дней +в подарок новинка пробник "Grape"'}
 ]
 
 
@@ -34,7 +34,7 @@ const ProductList = () => {
             },
             body: JSON.stringify(data)
         })
-    }, [])
+    }, [addedItems, queryId])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
